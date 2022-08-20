22eb8d39e71a5d2f3a1c0c36bc1e3a5f4c7e8e0f
Junho Yeo <i@junho.io>
2022-08-20 22:51:45 +0900

    Add buttons in `Navigation`

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,16 +2,25 @@ import { Center, Flex } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
+import { ZEPStudioIcon } from './ZEPStudioIcon';
+
 const Navigation: React.FC = () => {
   return (
     <Flex h="68px" bg="#262A2E" color={'white'} p={3} justify={'space-between'}>
       <Center gap="12px">
-        <IconImage />
+        <ZEPStudioIcon icon="icon_prev_32" size={32} color="#adb5bd" />
         <GrayBreadcrumb>
           <Link to="/">ZEP studio</Link>
         </GrayBreadcrumb>
         <GrayBreadcrumb>/</GrayBreadcrumb>
         <TitleBreadcrumb>Create a new function</TitleBreadcrumb>
+      </Center>
+
+      <Center gap="19px">
+        <PublishButton>Publish</PublishButton>
+        <SquareButton>
+          <ZEPStudioIcon icon="icon_more_24" size={24} color="#ADB5BD" />
+        </SquareButton>
       </Center>
     </Flex>
   );
@@ -19,13 +28,6 @@ const Navigation: React.FC = () => {
 
 export default Navigation;
 
-const IconImage = styled.span`
-  width: 32px;
-  height: 32px;
-  display: flex;
-  background-color: #adb5bd;
-  mask-image: url('/assets/icons/icon_prev_32.svg');
-`;
 const GrayBreadcrumb = styled.span`
   /* subtitle/02 */
   font-family: 'Pretendard';
@@ -52,4 +54,42 @@ const TitleBreadcrumb = styled.span`
   /* gray/00 */
 
   color: #ffffff;
+`;
+
+const PublishButton = styled.button`
+  background: #6559f6;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 13px 26px;
+
+  /* body/01-bold */
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  letter-spacing: 0.0125em;
+
+  /* lightTheme/grayscale/gray0 */
+
+  color: #f8f9fa;
+`;
+const SquareButton = styled.button`
+  width: 45px;
+  height: 45px;
+
+  /* gray/04 */
+
+  border: 1.5px solid #4d5359;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
