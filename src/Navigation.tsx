58c3e48e7d3575f6dfa8c441f2c8ca99f2e55ff1
Junho Yeo <i@junho.io>
2022-08-20 22:18:19 +0900

    Add icons, Style `Navigation`

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,24 +1,55 @@
-import { Button, Center, Flex, Stack, Text } from '@chakra-ui/react';
+import { Center, Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <Flex
-      h="68px"
-      bg={'nav.dark'}
-      color={'white'}
-      p={3}
-      justify={'space-between'}
-    >
-      <Stack alignItems={'center'}>
-        <Center>
-          <Text fontSize={24}>ZEP Studio</Text>
-        </Center>
-      </Stack>
-      <Stack>
-        <Button bg={'blue.500'}>Publish</Button>
-      </Stack>
+    <Flex h="68px" bg="#262A2E" color={'white'} p={3} justify={'space-between'}>
+      <Center gap="12px">
+        <IconImage />
+        <GrayBreadcrumb>
+          <Link to="/">ZEP studio</Link>
+        </GrayBreadcrumb>
+        <GrayBreadcrumb>/</GrayBreadcrumb>
+        <TitleBreadcrumb>Create a new function</TitleBreadcrumb>
+      </Center>
     </Flex>
   );
 }
 
 export default Navigation;
+
+const IconImage = styled.span`
+  width: 32px;
+  height: 32px;
+  display: flex;
+  background-color: #adb5bd;
+  mask-image: url('/assets/icons/icon_prev_32.svg');
+`;
+const GrayBreadcrumb = styled.span`
+  /* subtitle/02 */
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 29px;
+  /* identical to box height */
+
+  /* gray/03 */
+
+  color: #adb5bd;
+`;
+const TitleBreadcrumb = styled.span`
+  /* subtitle/02 */
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 29px;
+  /* identical to box height */
+
+  /* gray/00 */
+
+  color: #ffffff;
+`;
